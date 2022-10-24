c38e038a3e777e63b4603483d625bd9def8a3799
Emanuel Berger <emanuelberger49@gmail.com>
2022-10-24 20:05:13 +0200

    beauty changes

--- small bot/drive/drive_stepper_TB6600.ino
+++ small bot/drive/drive_stepper_TB6600.ino
@@ -1,35 +1,31 @@
 int Index;
 
-void setup()
-{
-pinMode(6, OUTPUT); //Enable
-pinMode(5, OUTPUT); //Puls
-pinMode(4, OUTPUT); //Direction
+void setup() {
+    pinMode(6, OUTPUT); //Enable
+    pinMode(5, OUTPUT); //Puls
+    pinMode(4, OUTPUT); //Direction
 
-digitalWrite(6,LOW);
+    digitalWrite(6,LOW);
 }
 
-void loop()
-{
-digitalWrite(4,HIGH);
+void loop() {
+    digitalWrite(4,HIGH);
 
-for(Index = 0; Index < 5000; Index++)
-{
-digitalWrite(5,HIGH);
-delayMicroseconds(500);
-digitalWrite(5,LOW);
-delayMicroseconds(500);
-}
-delay(1000);
+    for(Index = 0; Index < 5000; Index++) {
+        digitalWrite(5,HIGH);
+        delayMicroseconds(500);
+        digitalWrite(5,LOW);
+        delayMicroseconds(500);
+    }
+    delay(1000);
 
-digitalWrite(4,LOW);
+    digitalWrite(4,LOW);
 
-for(Index = 0; Index < 5000; Index++)
-{
-digitalWrite(5,HIGH);
-delayMicroseconds(500);
-digitalWrite(5,LOW);
-delayMicroseconds(500);
-}
-delay(1000);
+    for(Index = 0; Index < 5000; Index++) {
+        digitalWrite(5,HIGH);
+        delayMicroseconds(500);
+        digitalWrite(5,LOW);
+        delayMicroseconds(500);
+    }
+    delay(1000);
 }
